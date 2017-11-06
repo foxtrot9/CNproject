f1aae80f9f27c45d304cfc38da3c54a7bfa43565
Mit Naria <mit4dev@gmail.com>
2017-11-06 17:40:31 +0530

    Add bare minimum graph data structure.

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int nodes;	// Number of nodes
+	int rows;	// Number of rows
+	int columns;	// Number of columns
+	float **adjMat;	// Adjancy Matrix
+} Graph;
+
+Graph makeGraph(int rows, int columns) {
+	Graph g1;
+	g1.nodes = 100;
+	g1.rows = rows;
+	g1.columns = columns;
+	g1.adjMat = (float **) malloc(rows*columns);
+	return g1;
+}
 
 int main() {
 	printf("Hello World!\n");
