526cc4426ced04f027c9c1f8c4ee171ff7b1ca87
Mit Naria <mit4dev@gmail.com>
2017-11-06 17:54:24 +0530

    Add normalize function and bug fixes.
    1. rows and columns variable isn't needed in Graph data structure.
    2. Add addEdge function having standard network theory presentation.
    3. Add normalize Graph function.

--- main.c
+++ main.c
@@ -3,18 +3,34 @@
 
 typedef struct {
 	int nodes;	// Number of nodes
-	int rows;	// Number of rows
-	int columns;	// Number of columns
-	float **adjMat;	// Adjancy Matrix
+	float **adjMat;	// Adjacency Matrix
 } Graph;
 
-Graph makeGraph(int rows, int columns) {
+Graph makeGraph(int nodes) {
 	Graph g1;
-	g1.nodes = 100;
-	g1.rows = rows;
-	g1.columns = columns;
-	g1.adjMat = (float **) malloc(rows*columns);
+	g1.nodes = nodes;
+	g1.adjMat = (float **) malloc(nodes*nodes);
 	return g1;
+}
+
+void addEdge(Graph g, int source, int target, float edgeValue) {
+	g.adjMat[target][source] = edgeValue;
+}
+
+void normalizeGraph(Graph g) {
+	int i, j;
+	float sum = 0;
+	for (i = 0; i < g.nodes; ++i) {
+		sum = 0;
+		for (j = 0; j < g.nodes; ++j) {
+			sum += g.adjMat[i][j];
+		}
+		if(sum > 0.001f) {
+			for (j = 0; j < g.nodes; ++j) {
+				g.adjMat[i][j] = g.adjMat[i][j] / sum;
+			}
+		}
+	}
 }
 
 int main() {
